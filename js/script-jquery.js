867164e357fb533afe40bf009fc30f5de52e3927
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2017-12-26 20:36:52 -0700

    Added search feature to robots page

--- js/script-jquery.js
+++ js/script-jquery.js
@@ -1,7 +1,26 @@
 $(document).ready(() => {
-    $("#fullname, #email, #yloc, #yquest").on("blur", function() {
-        $(this).removeClass("highlight");
-    }).on("focus", function() {
-        $(this).addClass("highlight");
+    /**
+     * This will highligh contact form input for the user to
+     * know what input box they are in.
+     */
+    $('#fullname, #email, #yloc, #yquest').on('blur', function() {
+        $(this).removeClass('highlight');
+    }).on('focus', function() {
+        $(this).addClass('highlight');
+    });
+    /**
+     * This will allow the user to serach the robots page and it will
+     * hide and show robots according to the user's search.
+     */
+    $('.searchbox').keyup(function() {
+        var text = $(this).val();
+
+        $('article').parent().hide();
+
+        $('article').each(function() {
+            if ($(this).text().toLowerCase().indexOf("" + text + "") != -1) {
+                $(this).parent().show();
+            }
+        });
     });
 });
